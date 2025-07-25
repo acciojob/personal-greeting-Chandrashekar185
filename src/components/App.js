f2f1a0deaa2cd2a3b00ad447828708a0e7c1654b
Chandrashekar185 <chanduminny32@gmail.com>
2025-07-25 23:57:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 
 import React from "react";
 import './../styles/App.css';
-import GreetingComponent from './Greeting.jsx';
+
 
 const App = () => {
+  const [name, setName] = useState('');
   return (
     <div>
         {/* Do not remove the main div */}
-    <GreetingComponent>
+    <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Enter your name"
+      />
+      
+      {name.trim() !== '' && <h3>Hello, {name}!</h3>}
+    
     </div>
   )
 }
