0dae156a6e1d68e8cf630df9245d0ee0d6b7fd9f
Chandrashekar185 <chanduminny32@gmail.com>
2025-07-25 23:45:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
 import React from "react";
 import './../styles/App.css';
+import GreetingComponent from './Greeting.jsx';
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+    <GreetingComponent>
     </div>
   )
 }
